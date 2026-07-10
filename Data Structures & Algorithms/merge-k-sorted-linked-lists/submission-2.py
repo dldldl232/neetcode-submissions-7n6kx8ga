711ed893891ca7844436a52e6f8546c790b16be2
Seoyeon Kim <140609589+dldldl232@users.noreply.github.com>
2026-07-10 03:59:43 -0700

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+"""
+Algorithm:
+1. push the head node of every non-empty list into a min-heap (size <= k)
+2. pop the smallest node from the heap - that's the next node in your merged result
+3. if that node has a .next, push .next into the heap
+4. repeat until the heap is empty
+"""
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # we have to merge sorted linked list
+        heap = [] # now we can retrieve the smallest value
+
+        # step 1
+        # we use counter as tiebreaker as ListNode isn't comparable
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+        # even though we push tuples we compare them: element by element, left to right
+        # i is used as tiebreak if nodes have equal values
+        # we need node: the actual node, we we need later so we can access .next
+
+        dummy = ListNode()
+        curr = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap) # automatically pops the smallest one
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+            
+        
+        return dummy.next
