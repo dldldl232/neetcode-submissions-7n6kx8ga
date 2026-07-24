1e9a1a3ca530d2a2a2629aa6f4334467dae63bf6
Seoyeon Kim <140609589+dldldl232@users.noreply.github.com>
2026-07-24 02:25:42 -0700

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # Assume that division between integers always truncates toward zero.
+        # -> means we have to use int(a/b)
+        if not tokens:
+            return 0
+
+        if len(tokens) == 1:
+            return int(tokens[0])
+            
+        stk = []
+        
+        for c in tokens:
+            if c == '+':
+                elem2 = stk.pop()
+                elem1 = stk.pop()
+
+                output = elem1 + elem2
+                stk.append(output)
+            
+            elif c == '-':
+                elem2 = stk.pop()
+                elem1 = stk.pop()
+                
+                output = elem1 - elem2
+                stk.append(output)
+
+            elif c == '*':
+                elem2 = stk.pop()
+                elem1 = stk.pop()
+
+                output = elem1 * elem2
+                stk.append(output)
+            
+            elif c == "/":
+                elem2 = stk.pop()
+                elem1 = stk.pop()
+
+                output = int(elem1 / elem2)
+                stk.append(output)
+            
+            else:
+                stk.append(int(c))
+        
+        return output
