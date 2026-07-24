13e78a1b73df37953218270fb3f9027f2467eac7
Seoyeon Kim <140609589+dldldl232@users.noreply.github.com>
2026-07-24 00:48:10 -0700

    Add: validate-parentheses - submission-19

---
+++ Data Structures & Algorithms/validate-parentheses/submission-19.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+
+        stk = deque()
+
+        if len(s) == 1:
+            return False
+        
+        if not len(s):
+            return True
+
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stk.append(char)
+            elif char in d:
+                if len(stk) == 0:
+                    stk.append(char)
+                elif stk[-1] == d[char]:
+                    stk.pop()
+                else:
+                    stk.append(char)
+        
+        return len(stk) == 0
+
+
+        
